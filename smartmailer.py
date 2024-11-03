882d20e3750e779627d6ba60f1f18339623597f1
pwjj2000 <perrywongjunjie@gmail.com>
2024-11-03 17:40:19 +0800

    Count and print number of emails sent to each department

--- smartmailer.py
+++ smartmailer.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import smtplib
 import time
+from tabulate import tabulate
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
@@ -22,6 +23,10 @@ def setup(dept):
     curr_dir = os.path.dirname(os.path.realpath(__file__))
     csv_filename = os.path.join(curr_dir, 'maildata.csv')
     maildata_df = pd.read_csv(csv_filename)
+    
+    # Counter that keeps track of emails sent for each department
+    count = {d: 0 for d in set(maildata_df['DEPARTMENT'].values)}
+
     if dept != 'all':
         maildata_df = maildata_df[maildata_df['DEPARTMENT'] == dept]
     maildata_df.reset_index()
@@ -39,7 +44,7 @@ def setup(dept):
     with open(txt_filename, 'r') as f:
         email_contents = f.readlines()
         
-    return maildata_df, email_contents
+    return maildata_df, email_contents, count
 
 # Check if email address is valid or not
 def is_valid_email(email):
@@ -67,7 +72,7 @@ def send_email(s, subject, body, receiver):
 
 if __name__ == "__main__":
     dept = sys.argv[1]
-    maildata_df, email_contents = setup(dept)
+    maildata_df, email_contents, count = setup(dept)
 
     with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as s:
         s.starttls()
@@ -78,4 +83,10 @@ if __name__ == "__main__":
             email, name, department = row['EMAIL'], row['NAME'], row['DEPARTMENT']
             subject, body = prepare_email(email_contents, email, name, department)
             send_email(s, subject, body, email)
+            count[department] += 1
             time.sleep(DELAY)
+
+    table = tabulate(count.items(), headers=['Department', 'Count'])
+
+    print('Number of emails sent (by Department):')
+    print(table)
